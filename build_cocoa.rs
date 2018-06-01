ee891b2b67d9f87cc44fc890b548ccc4dee99b68
olga <olga@olka-air.w.mifi>
2018-06-01 16:27:30 +0200

    Cocoa copy framework to out (raw)

--- build_cocoa.rs
+++ build_cocoa.rs
@@ -1,6 +1,8 @@
 extern crate cc;
+extern crate fs_extra;
 
 use std::env;
+use std::path;
 use std::process::Command;
 
 pub fn main() {
@@ -20,7 +22,16 @@ pub fn main() {
         
     let mut framework_dir = env::current_dir().unwrap();
     framework_dir.push("build/Release");    
-    println!("cargo:rustc-link-search=framework={}", framework_dir.display());    
+    println!("cargo:rustc-link-search=framework={}", framework_dir.display());   
+    
+    let mut out_dir = path::PathBuf::from(env::var("OUT_DIR").unwrap());
+    out_dir.push("Frameworks");
+    
+    fs_extra::dir::create_all(out_dir.clone(), true).unwrap();
+    
+    let mut opts = fs_extra::dir::CopyOptions::new();
+    opts.overwrite = true;
+    fs_extra::dir::copy(framework_dir, out_dir, &opts).unwrap();
         
     /*env::set_current_dir("cocoa").expect("Could not change dir");    
 
